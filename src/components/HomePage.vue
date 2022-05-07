c36c2b3601ce10693f4ea40a0daf8bd749fa62c8
omer <ffaruk.yamak@gmail.com>
2022-05-07 19:30:16 +0100

    create homepage structure

---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <AddTask />
+  <AllTasks />
+</template>
+
+<script>
+import AddTask from "./AddTask.vue";
+import AllTasks from "./AllTasks.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    AddTask,
+    AllTasks,
+  },
+};
+</script>
+
+<style></style>
