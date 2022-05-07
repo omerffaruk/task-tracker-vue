23278324dd47a6155ccada7550197fa46aacea35
omer <ffaruk.yamak@gmail.com>
2022-05-07 19:29:43 +0100

    create initial header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container header">
+    <h1>Task Tracker</h1>
+    <div>
+      <button>Close</button>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
