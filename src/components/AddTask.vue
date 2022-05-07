f78dc58c57d3b0b326d6493a91a451ca17528da0
omer <ffaruk.yamak@gmail.com>
2022-05-07 19:30:34 +0100

    create add task structure

---
+++ src/components/AddTask.vue
@@ -0,0 +1,23 @@
+<template>
+  <form class="add-task-form">
+    <div class="form-control">
+      <label for="task-name">Task</label>
+      <input type="text" id="task-name" placeholder="Task" v-model="taskName" />
+    </div>
+    <div class="form-control">
+      <label for="task-date">Date & Time</label>
+      <input
+        type="text"
+        id="task-date"
+        v-model="taskDate"
+        placeholder="Date & Time"
+      />
+    </div>
+
+    <button type="submit">Save Task</button>
+  </form>
+</template>
+
+<script></script>
+
+<style></style>
